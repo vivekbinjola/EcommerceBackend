60e87ede21552fcbb95cb2e8f91de11baa9ddbda
vivek <vivekbinjola23@gmail.com>
2025-03-06 22:20:23 +0530

    notification service finished

--- notification/src/main/java/com/vivek/notification/Kafka/OrderConfirmation.java
+++ notification/src/main/java/com/vivek/notification/Kafka/OrderConfirmation.java
@@ -3,10 +3,14 @@ package com.vivek.notification.Kafka;
 import com.vivek.notification.DTO.Customer;
 import com.vivek.notification.DTO.PaymentMethod;
 import com.vivek.notification.DTO.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public record OrderConfirmation(
 
         String orderReference,
